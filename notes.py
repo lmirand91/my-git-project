9c59f9e39e037267f669a7076b1d1de49195f8f7
lmirand91 <lmirand91@gmail.com>
2021-11-15 00:19:35 -0600

    Updated with note file

---
+++ notes.py
@@ -0,0 +1,16 @@
+ #     git init                         //initializes repository
+ #     git status                       //shows current state of repository
+ #     dir                              //shows all available files in directory (doesn't include hidden unless change setting)
+ #     git add .                        //updates repository with all untracked files, as well as changed files
+ #     git add my_file.py               //updates respository with specific file by including path to file
+ #     git rm --cached my_file.py       //removed cached file from queue
+ #     git commit -m "Initial commit"   //m for message, commits file to git repository (creates a version)
+ #     git remote add origin https://github.com/lmirand91/my-git-project.git 
+ #     //connects github (remote) repository to local repository --origin-represents named remote
+ #     git remote -v                    //lets you view the named origin with fetch and push shown
+ #     git branch -M main               //specifies main branch
+ #     git push -u origin main          //pushes to remote repository
+ #     git log                          //shows all previous activity of repository
+
+ #  Following commands are git commands, that aren't specific to github
+ #  On github, new repository, needs a name unique to your set of repositories
